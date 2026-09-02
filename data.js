a5308140828f44d94564990cc0e224ffe733309c
beam-mods <jacksonwing11@gmail.com>
2026-09-02 16:08:34 -0400

    Update car mod details for GR Supra and NA Miata

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 const beamMods = [
     {
-        id: "cherrier-ffx",
-        name: "GR Supra - Redline Autos",
+        id: "grsupra",
+        name: "GR Supra - Redline Auto",
         category: "Supercars",
         image: "https://static.wixstatic.com/media/252168_551c98a21efe4f8ba3799ef19ec41f2d~mv2.png/v1/fill/w_1853,h_796,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/252168_551c98a21efe4f8ba3799ef19ec41f2d~mv2.png", // 🆕 Path to your car screenshot
         size: "142 MB",
-        description: "Fixed for 0.39. The GR Supra is Toyota's newest addition to the Supra lineup. Included in the mod is the 2JZ-GTE engine making 320hp from factory.",
+        description: "Fixed for 0.39. Highly detailed GR Supra mod from Redline Auto, lots of configs and parts.",
         qiwiLink: "YOUR_QIWI_LINK_HERE_1"
     },
     {
-        id: "gavril-vertex",
-        name: "Gavril Vertex V8 Twin Turbo",
+        id: "miatana",
+        name: "NA Miata - Redline Auto",
         category: "Supercars",
-        image: "images/vertex.jpg", // 🆕 Path to your car screenshot
+        image: "https://static.wixstatic.com/media/252168_290c5d2af5b543739bd91e5902a85af8~mv2.jpg/v1/fill/w_452,h_242,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/252168_290c5d2af5b543739bd91e5902a85af8~mv2.jpg", // 🆕 Path to your car screenshot
         size: "210 MB",
-        description: "High-tier premium custom layout with custom drift geometries and adjustable suspension configurations.",
+        description: "Fixed for 0.39. NA Miata, lots of configs and parts. ",
         qiwiLink: "YOUR_QIWI_LINK_HERE_2"
     }
 ];
